c2381b49c9425e19f5a68a58b22c04912c1460f7
ggk <ggk>
2026-01-02 12:36:28 +0100

    bug fix

--- stt_api.py
+++ stt_api.py
@@ -12,14 +12,6 @@ def get_text():
   with open(file_path, "r", encoding="utf-8") as f:
     return f.read()    
 
-
-def get_text():
-  base_dir = os.path.dirname(os.path.abspath(__file__))
-  file_path = os.path.join(base_dir, 'text_to_convert.txt')
-
-  with open(file_path, "r", encoding="utf-8") as f:
-
-
 text = get_text()
 filename = input("enter download file name ")
 
