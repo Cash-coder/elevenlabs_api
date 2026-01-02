27181a125704a7d0f33bdc59b5c7b940d3da4fc7
ggk <ggk>
2026-01-02 14:11:48 +0100

    added fn

--- stt_api.py
+++ stt_api.py
@@ -15,21 +15,39 @@ def get_text():
 
 
 filename = input("enter download file name ")
-print("creating audio output")
+print("creating audio output ... ")
 
 client = ElevenLabs(
-    # base_url="https://api.elevenlabs.io"
+  # base_url="https://api.elevenlabs.io"
   api_key=os.getenv("ELEVENLABS_API_KEY"),
 )
 
 audio = client.text_to_speech.convert(
     text=get_text(),
-    voice_id="JBFqnCBsd6RMkjVDRZzb",
+    # voice_id="N2lVS1w4EtoT3dr4eOWO", #w40k
+    voice_id="EiNlNiXeDU1pqqOPrYMO",
     # model_id="eleven_multilingual_v2",
-    model_id="eleven_flash_v2.5",
+    model_id="eleven_flash_v2_5",
     output_format="mp3_44100_128",
-    # voice_id="JBFqnCBsd6RMkjVDRZzb",
 )
 
 # play(audio)
-save(audio, filename.strip() + '.mp3')
+save(audio, 'results/' + filename.strip() + '.mp3')
+
+# print credits used
+# import requests
+
+# def print_last_query_credits(api_key):
+#     headers = {"xi-api-key": api_key}
+#     response = requests.get("https://api.elevenlabs.io/v1/user/subscription", headers=headers)
+    
+#     if response.status_code == 200:
+#         data = response.json()
+#         print(f"Credits used: {data.get('character_count', 'N/A')}/{data.get('character_limit', 'N/A')}")
+#     else:
+#         print(f"Error: {response.status_code}")
+
+# # Usage
+# api_key = "your_api_key_here"
+# print_last_query_credits(api_key)
+
