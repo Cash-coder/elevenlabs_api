35242304d906eed410546e792bd43a1fee85e594
ggk <ggk>
2026-01-02 12:21:59 +0100

    working

--- stt_api.py
+++ stt_api.py
@@ -4,18 +4,33 @@ from elevenlabs.client import ElevenLabs
 from elevenlabs.play import play
 from elevenlabs import save
 
+
+TEXT_FILEPATH = ""
+
 load_dotenv()
 
 elevenlabs = ElevenLabs(
   api_key=os.getenv("ELEVENLABS_API_KEY"),
 )
 
+def get_text():
+  base_dir = os.path.dirname(os.path.abspath(__file__))
+  file_path = os.path.join(base_dir, 'text_to_convert.txt')
+
+  with open(file_path, "r", encoding="utf-8") as f:
+    return f.read()    
+
+
+text = get_text()
+
+filename = input("enter download file name ")
+
 audio = elevenlabs.text_to_speech.convert(
-    text="The first move is what sets everything in motion.",
+    text=text,
     voice_id="JBFqnCBsd6RMkjVDRZzb",
     model_id="eleven_multilingual_v2",
     output_format="mp3_44100_128",
 )
 
 # play(audio)
-save(audio, 'test.mp3')
+save(audio, filename.strip() + '.mp3')
